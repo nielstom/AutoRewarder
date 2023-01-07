2af0139e66ce8f1263069e8806eed44cc66d561b
nielstom <121768626+nielstom@users.noreply.github.com>
2023-01-07 13:43:11 -0600

    Cleanup
    Comments and a sleep(1) after searches complete

--- AutoRewarder.py
+++ AutoRewarder.py
@@ -1,12 +1,14 @@
 import keyboard
 from time import sleep
 
+# Open edge
 keyboard.press_and_release('win+r')
 sleep(1)
 keyboard.write('microsoft-edge:')
 keyboard.press_and_release('enter')
 sleep(1)
 
+# Run 10 bing searches from edge
 nSearches = 10
 for i in range(nSearches):
     keyboard.write('search ' + str(i))
@@ -15,4 +17,6 @@ for i in range(nSearches):
     keyboard.press_and_release('ctrl+t')
     sleep(0.5)
 
+# Close edge
+sleep(1)
 keyboard.press_and_release('alt+f4')
